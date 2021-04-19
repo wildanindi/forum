b0fe8e48628d3c68f9ccae19bcab71f0ee534116
Wildan Indi <wildanindi2@gmail.com>
2021-04-19 10:11:58 +0700

    fix like on pg

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -3,10 +3,12 @@ class ForumThreadsController < ApplicationController
 
     def index
         if params[:search]
-            @threads = ForumThread.where('title like ?', "%#{params[:search]}%").paginate(per_page: 5, page: params[:page])
+            @threads = ForumThread.where('title ilike ?', "%#{params[:search]}%")
         else
-          @threads = ForumThread.order(sticky_order: :asc).order(id: :desc).paginate(per_page: 5, page: params[:page])
+          @threads = ForumThread.order(sticky_order: :asc).order(id: :desc)
         end
+
+        @threads.paginate(per_page: 5, page: params[:page])
     end
 
     def show
